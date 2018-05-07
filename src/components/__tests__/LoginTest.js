5de1cf7aefee9c3b417c97a2f682cc33db6a1861
Alex Gaudiosi <alex.gaudiosi@simplybusiness.co.uk>
2018-05-07 17:20:01 +0100

    configured enzyme/jest with es7

--- src/components/__tests__/LoginTest.js
+++ src/components/__tests__/LoginTest.js
@@ -1,3 +1,41 @@
-import React from 'react';
-import { shallow, mount, render } from 'enzyme';
-import Login from '../Login';
+import React from 'react'
+import { shallow, mount, render } from 'enzyme'
+import Login from '../Login'
+// describe what we are testing
+describe('Login Component', () => {
+  // make our assertion and what we expect to happen
+  it('should render without throwing an error', () => {
+    expect(shallow(<Login />).exists(<form className="login" />)).toBe(true)
+  })
+
+  it('renders a email input', () => {
+    expect(shallow(<Login />).find('#email').length).toEqual(1)
+  })
+  it('renders a password input', () => {
+    expect(shallow(<Login />).find('#password').length).toEqual(1)
+  })
+})
+
+describe('Email input', () => {
+  it('should respond to change event and change the state of the Login Component', () => {
+    const wrapper = shallow(<Login />)
+    wrapper
+      .find('#email')
+      .simulate('change', {
+        target: { name: 'email', value: 'blah@gmail.com' },
+      })
+
+    expect(wrapper.state('email')).toEqual('blah@gmail.com')
+  })
+})
+
+describe('Password input', () => {
+  it('should respond to change event and change the state of the Login Component', () => {
+    const wrapper = shallow(<Login />)
+    wrapper
+      .find('#password')
+      .simulate('change', { target: { name: 'password', value: 'cats' } })
+
+    expect(wrapper.state('password')).toEqual('cats')
+  })
+})
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,5 @@
-const Enzyme = require('enzyme');
-// this is where we reference the adapter package we installed
-// earlier
-const EnzymeAdapter = require('enzyme-adapter-react-15');
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 // This sets up the adapter to be used by Enzyme
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+Enzyme.configure({ adapter: new Adapter() })
